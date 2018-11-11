17b72d813f4fe9e0812a3417f4ddea3186d870a6
Almir Junior <almirjr944@gmail.com>
2018-11-10 23:07:55 -0200

    email HTML

--- src/main/java/com/almirjr94/cursomc/service/EmailService.java
+++ src/main/java/com/almirjr94/cursomc/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.almirjr94.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.almirjr94.cursomc.domain.Pedido;
@@ -8,5 +10,8 @@ public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
